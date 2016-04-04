e774f0412efdd8a851f921b29d35128562b40fed
David Gageot <david@gageot.net>
2016-04-04 09:13:57 +0200

    Move to the go module
    Signed-off-by: David Gageot <david@gageot.net>

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,6 @@
 $Shell = $Host.UI.RawUI
 $Shell.WindowTitle="PowerShell"
 
-$env:GOPATH="${env:USERPROFILE}\go"
-
 Import-Module Go\*.psm1
 Import-Module Prompt\*.psm1
 Import-Module Secret\*.psm1
--- Modules/Go/gogo.psm1
+++ Modules/Go/gogo.psm1
@@ -1,3 +1,5 @@
+$env:GOPATH="${env:USERPROFILE}\go"
+
 function gogo {
 	$project = $args[0]
 	set-location "${env:USERPROFILE}/go/src/github.com/docker/$project"
