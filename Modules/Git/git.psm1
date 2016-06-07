b147aac194ef34cf1995d608524612be76fea09b
David Gageot <david@gageot.net>
2016-06-07 09:09:53 +0200

    Use choco installed poshgit
    Signed-off-by: David Gageot <david@gageot.net>

--- Modules/Git/git.psm1
+++ Modules/Git/git.psm1
@@ -1,3 +1,6 @@
-. (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
+if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
 
-Start-SshAgent -Quiet
+. 'C:\tools\poshgit\dahlbyk-posh-git-19f3f2e\profile.example.ps1'
+
+Rename-Item Function:\Prompt PoshGitPrompt -Force
+function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
