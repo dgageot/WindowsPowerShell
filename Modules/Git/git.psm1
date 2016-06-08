54c172a6fbc87c0fd33b046ee58d80010cb09c63
David Gageot <david@gageot.net>
2016-06-08 09:55:31 +0200

    Simpler git configuration
    Signed-off-by: David Gageot <david@gageot.net>

--- Modules/Git/git.psm1
+++ Modules/Git/git.psm1
@@ -1,9 +1,4 @@
 New-Item -ItemType SymbolicLink -Path $env:USERPROFILE -Name .gitconfig -Target "$env:USERPROFILE\Documents\WindowsPowerShell\git\gitconfig.symlink" -Force
 New-Item -ItemType SymbolicLink -Path $env:USERPROFILE -Name .gitignore -Target "$env:USERPROFILE\Documents\WindowsPowerShell\git\gitignore.symlink" -Force
 
-if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
-
 . 'C:\tools\poshgit\dahlbyk-posh-git-19f3f2e\profile.example.ps1'
-
-Rename-Item Function:\Prompt PoshGitPrompt -Force
-function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
